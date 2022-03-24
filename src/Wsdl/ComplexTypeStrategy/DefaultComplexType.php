08d2f53d7b542f87a411e1d0bd3bdfe3f956b6be
desarrollo-ti <43783956+desarrollo-ti@users.noreply.github.com>
2022-03-24 16:23:59 +0100

    Update DefaultComplexType.php
    Corrección de bug nillable

--- src/Wsdl/ComplexTypeStrategy/DefaultComplexType.php
+++ src/Wsdl/ComplexTypeStrategy/DefaultComplexType.php
@@ -66,7 +66,7 @@ class DefaultComplexType extends AbstractComplexTypeStrategy
                 $element->setAttribute('type', $this->getContext()->getType(trim($matches[1][0])));
 
                 // If the default value is null, then this property is nillable.
-                if (isset($defaultProperties[$propertyName]) && $defaultProperties[$propertyName] === null) {
+                if (array_key_exists($propertyName, $defaultProperties) && $defaultProperties[$propertyName] === null) {
                     $element->setAttribute('nillable', 'true');
                 }
 
